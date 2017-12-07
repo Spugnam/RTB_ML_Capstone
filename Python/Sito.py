80f6832008563d7318ad78b0a9cb4d5108916c89
Spugnam <quentin.picard@gmail.com>
2017-12-06 22:11:20 -0500

    working Sito.py version

--- Python/Sito.py
+++ Python/Sito.py
@@ -3,19 +3,19 @@ from csv import DictReader
 from math import exp, log, sqrt
 import mmh3
 import pandas as pd
-
+from sklearn.feature_selection import mutual_info_classif
 
 # parameters #################################################################
 
-train = '../data/train.csv'  # path to training file
-test = '../data/test.csv'  # path to testing file
+train = '../data/mini_train.csv'  # path to training file
+test = '../data/mini_test.csv'  # path to testing file
 
 # create test panda df
 test_df = pd.read_csv(test)
 test_df = test_df.drop(test_df.columns[0], axis=1)
 
 D = 2 ** 20   # number of weights use for learning
-alpha = .1    # learning rate for sgd optimization
+alpha = .1    # learning rate for stochastic gradient descent (sgd) optimization
 
 
 # function definitions #######################################################
@@ -41,7 +41,8 @@ def logloss(p, y):
 def get_x(csv_row, D):
     x = [0]  # 0 is the index of the bias term
     for key, value in csv_row.items():
-        index = int(str(mmh3.hash(value + key[1:])), 16) % D  # 2nd argument of int means base 16 - basic hash
+        # index = int(str(mmh3.hash(value + key[1:])), 16) % D  # int in base 16
+        index = mmh3.hash(value + key[1:]) % D
         x.append(index)
     return x  # x contains indices of features that have a value of 1
 
@@ -75,7 +76,7 @@ def update_w(w, n, x, p, y):
         # alpha / (sqrt(n) + 1) is the adaptive learning rate heuristic
         # (p - y) * x[i] is the current gradient
         # note that in our case, if i in x then x[i] = 1
-        w[i] -= (p - y) * alpha / (sqrt(n[i]) + 1.)
+        w[i] -= (p - y) * alpha / (sqrt(n[i]) + 1.)  # gradient descent formula with adaptive alpha
         n[i] += 1.
 
     return w, n
@@ -93,7 +94,7 @@ for t, row in enumerate(DictReader(open(train))):
     y = 1. if row['clicked'] == '1' else 0.
 
     del row['clicked']  # can't let the model peek the answer
-    del row['day']  # only used to split train/ test, timestamp_weekday for learning
+    # del row['day']  # only used to split train/ test, timestamp_weekday for learning
 
     # main training procedure
     # step 1, get the hashed features
@@ -104,21 +105,21 @@ for t, row in enumerate(DictReader(open(train))):
 
     # for progress validation, useless for learning our model
     loss += logloss(p, y)
-    if t % 1000000 == 0 and t > 1:
+    if t % 1e6 == 0 and t > 1:
         print('%s\tencountered: %d\tcurrent logloss: %f' % (
             datetime.now(), t, loss / t))
 
     # step 3, update model with answer
     w, n = update_w(w, n, x, p, y)
 
-# Add predictions column
+# save probability column
 test_df['probability'] = 0
 for t, row in enumerate(DictReader(open(test))):
     x = get_x(row, D)
     p = get_p(x, w)
     test_df.loc[t, 'probability'] = p
 
-# testing (build kaggle's submission file)
+# Saving to file
 with open(str(datetime.now()).split(".")[0] + 'predictions.csv', 'w') as predictions:
     predictions.write('Id,Predicted\n')
     for t, row in enumerate(DictReader(open(test))):
@@ -127,5 +128,33 @@ with open(str(datetime.now()).split(".")[0] + 'predictions.csv', 'w') as predict
         p = get_p(x, w)
         _ = predictions.write('%s,%f\n' % (Id, p))
 
-loss/test_df.shape[0]
-test_df.head()
+# Mutual information ############################################
+
+MI = mutual_info_classif(test_df.select_dtypes(include=['int64']),
+                         test_df['clicked'].values)
+MI_list = list()
+for i in range(len(MI)):
+    MI_list.append([test_df.select_dtypes(include=['int64']).columns[i], MI[i]])
+
+MI_list.sort(key=lambda x: -x[1])
+print(MI_list, "\n")
+# Results #######################################################
+
+# logloss
+from sklearn.metrics import log_loss
+print("log_loss: ",
+      log_loss(test_df['clicked'], test_df['probability'], eps=1e-15, normalize=True),
+      "\n")
+# Normalize = True -> return the mean loss per sample
+
+from sklearn.metrics import roc_auc_score
+print("roc_auc_score",
+      roc_auc_score(test_df['clicked'], test_df['probability'], average="macro"),
+      "\n")
+# roc_auc_score(['clicked'], test_df['probability'], average="weighted")
+
+from sklearn.metrics import average_precision_score
+print("average_precision_score",
+      average_precision_score(test_df['clicked'], test_df['probability'], average="macro"))
+
+test_df.loc[:, ['clicked', 'probability']].head(20)
